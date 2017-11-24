93745dd6573fbaa440490618d315f7daf9741d0a
MehdiTorkamani <33295235+MehdiTorkamani@users.noreply.github.com>
2017-11-24 09:48:18 +0000

    Minor changes to file size, load, colours, colliders
    Redundant files deleted.
    Load bar code changed. The bar now increases from 0 to 100 and LoadSceneAsync is called at start of loading bar process.
    Menu colours changed from black to grey.
    Border colliders reduced from 50+ to only 4.

--- SegwayZombie/Assets/Scripts/LoadLevel.cs
+++ SegwayZombie/Assets/Scripts/LoadLevel.cs
@@ -5,11 +5,11 @@ using UnityEngine.UI;
 
 public class LoadLevel : MonoBehaviour {
 
-	public GameObject loadScreen;
-	public Slider slider;
-	public Text loadText;
-
+	[SerializeField] GameObject loadScreen;
+	[SerializeField] Slider slider;
+	[SerializeField] Text loadText;
 	[SerializeField] GameObject instructions;
+	bool barComplete = false;
 
 	public void StartGame()
 	{
@@ -18,22 +18,48 @@ public class LoadLevel : MonoBehaviour {
 
 	public void LoadGame()
 	{
-		StartCoroutine(Load());
+		StartCoroutine (LoadBar ());
+	}
+
+	IEnumerator LoadBar()
+	{
+		instructions.SetActive (false);
+		loadScreen.SetActive (true);
+		int rand = Random.Range (5, 30);
+		for (int i = 0; i <= 100; i++)
+		{
+			slider.value = i / 100f;
+			loadText.text = i + "%";
+
+			//SceneManager.LoadSceneAsync causes the game to stop for a short time before operation.progress starts, so this short time should be a random point at start of load bar
+			if (i == rand)
+				StartCoroutine (Load());
+
+			yield return null;
+		}
+		barComplete = true;
 	}
 
 	IEnumerator Load()
 	{
 		AsyncOperation operation = SceneManager.LoadSceneAsync (1);
-		instructions.SetActive (false);
-		loadScreen.SetActive (true);
+		operation.allowSceneActivation = false;
 
+		while (!barComplete)
+		{
+			yield return null;
+		}
+
+		operation.allowSceneActivation = true;
+		/*
+		//Alternative method: The operation.progress jumped from 0 to 100 really quickly and didn't look right.
 		while (!operation.isDone)
 		{
 			float progress = Mathf.Clamp01 (operation.progress / 0.9f);
 			slider.value = progress;
 			loadText.text = (progress * 100f).ToString ("0") + "%";
-
 			yield return null;
 		}
+		*/
 	}
 }
